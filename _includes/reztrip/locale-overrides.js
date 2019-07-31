7733ebe470a6747939e4c7609d9dbcd3468c26c7
Hari Kumar <hracha@traveltripper.com>
2019-07-31 15:36:09 +0530

    CP added

--- _includes/reztrip/locale-overrides.js
+++ _includes/reztrip/locale-overrides.js
@@ -1,5 +1,9 @@
 var localeOverrides = {
   'offers': {
-    'dateFormat': 'MMM D'          
+    'dateFormat': 'MMM D'
+  },
+  'conversion-plus': {
+    'select_dates': 'Book Now',
+    'guaranteed_price': 'Best Rate Available'
   }
 }
